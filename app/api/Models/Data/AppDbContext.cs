a759c47b6fd4b38ddd2e08fcdd0634b85be8eb5c
ricardo didimo <ricardodidimodev@outlook.com>
2021-09-15 07:24:28 -0300

    Entities clean up after project scoped down

--- app/api/Models/Data/AppDbContext.cs
+++ app/api/Models/Data/AppDbContext.cs
@@ -12,16 +12,10 @@ namespace api.Models.Data
         }
 
         public DbSet<User> users { get; set; }
-        public DbSet<Category> categories { get; set; }
-        public DbSet<Link> links { get; set; }
-        public DbSet<Artist> artists { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.BuildUser();
-            builder.BuildCategory();
-            builder.BuildLink();
-            builder.BuildArtist();
         }
         
     }
--- app/api/Models/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace api.Models.Entities
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-    }
-}
