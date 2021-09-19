7422de1de3e3c3bf832cb4cc25f0e5d54d4d44c7
Ricardo Didimo <86539214+ricardodidimo@users.noreply.github.com>
2021-09-19 22:11:18 +0000

    Quick fix in UserServiceTest

--- app/tests/UserServiceTest.cs
+++ app/tests/UserServiceTest.cs
@@ -18,9 +18,9 @@ namespace tests
         private Mock<IHttpContextAccessor> httpContext = new Mock<IHttpContextAccessor>();
         private IConfiguration configuration = new ConfigurationBuilder()
             // Path for folder containing variables
-            .SetBasePath("C:\\Users\\Ricardo\\Desktop\\artful_api\\app\\tests")
+            .SetBasePath("")
             // File containing variables
-            .AddJsonFile("appsettings.Development.json")
+            .AddJsonFile("")
             .Build();
 
 
