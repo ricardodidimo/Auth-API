2536f0d8ca25fd4acb1145d7987e4cb73533cfb9
unknown <ricardodidimo21@gmail.com>
2023-03-25 22:03:18 -0300

    added FastEndpoint style class for 'get all users'

---
+++ app/api/Endpoints/GetUsers/GetUserResponse.cs
@@ -0,0 +1,10 @@
+﻿using api.Models.Views;
+using System.Collections.Generic;
+
+namespace api.Endpoints.GetUsers
+{
+    public class GetUserResponse
+    {
+        public List<UserViewModel> Data { get; set; }
+    }
+}
---
+++ app/api/Endpoints/GetUsers/GetUsersEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using api.Endpoints.GetUsers;
+using api.Models.Views;
+using api.Services;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace api.Endpoints.GetAllUsers
+{
+    public class GetUsersEndpoint : Endpoint<EmptyRequest, GetUserResponse>
+    {
+        public IUserService userService { get; set; }
+
+        public GetUsersEndpoint(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        public override void Configure()
+        {
+            Verbs(Http.GET);
+            Routes("/users");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+        {
+            var data = await userService.GetUsersAsync();
+            await SendAsync(new GetUserResponse()
+            {
+                Data = data
+            }, StatusCodes.Status200OK, ct);
+        }
+    }
+}
